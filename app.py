6a15096cd1af9fad64c51a8789444626e78948ca
projectmenia <143309217+projectmenia@users.noreply.github.com>
2023-08-27 13:31:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,29 +18,27 @@ if video_button_clicked:
     video_url = 'https://github.com/projectmenia/Romromtrip/raw/main/rom-rom-bhaiyo-system-paad-denge-deepak-kalal-meme-template-1280-ytshorts.savetube.me.mp4'
     st.video(video_url)
 
-    # Add a button to proceed after video ends
-    see_trip_button = st.button("See your RomRom trip")
-    if see_trip_button:
-        content_placeholder.empty()  # Clear the video content
+    content_placeholder.empty()  # Clear the video content
 
-        # Date Selection
-        if not df.empty:
-            unique_dates = df['Date'].dt.strftime("%d %B %Y").unique()
-            selected_date = st.selectbox("Select a date", unique_dates)
+    # Date Selection
+    if not df.empty:
+        unique_dates = df['Date'].dt.strftime("%d %B %Y").unique()
+        unique_dates = ['None'] + list(unique_dates)  # Add 'None' option
+        selected_date = st.selectbox("Select a date", unique_dates)
 
-            if selected_date:
-                # Filter data based on selected date
-                selected_data = df[df['Date'].dt.strftime("%d %B %Y") == selected_date]
+        if selected_date != 'None':
+            # Filter data based on selected date
+            selected_data = df[df['Date'].dt.strftime("%d %B %Y") == selected_date]
 
-                # Display information
-                if not selected_data.empty:
-                    st.write(f"Selected Date: {selected_date}")
-                    for index, row in selected_data.iterrows():
-                        st.write(f"  - Source: {row['source']}")
-                        st.write(f"  - Place: {row['destination']}")
-                        st.write(f"  - Mode: {row['mode']} - Budget: {row['BUDGET']}")
-                        st.write("----")  # Separate different travel details
-                else:
-                    st.write("No information available for the selected date.")
+            # Display information
+            if not selected_data.empty:
+                st.write(f"Selected Date: {selected_date}")
+                for index, row in selected_data.iterrows():
+                    st.write(f"  - Source: {row['source']}")
+                    st.write(f"  - Place: {row['destination']}")
+                    st.write(f"  - Mode: {row['mode']} - Total Budget: {row['BUDGET']}")  # Display total budget
+                    st.write("----")  # Separate different travel details
             else:
-                st.write("Please select a date.")
+                st.write("No information available for the selected date.")
+        else:
+            st.write("No date selected.")

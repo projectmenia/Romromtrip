dfc699d5f46acfde42a28395f156974686f1f4af
projectmenia <143309217+projectmenia@users.noreply.github.com>
2023-08-27 14:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,18 +50,12 @@ st.markdown(
 # Main App
 st.title("RomRom Trip Planner")
 
-# Video Pop-up
-video_url = 'https://github.com/projectmenia/Romromtrip/raw/main/rom-rom-bhaiyo-system-paad-denge-deepak-kalal-meme-template-1280-ytshorts.savetube.me.mp4'
-video_button_clicked = st.button("RomRom bhaiyo Click here")
-if video_button_clicked:
-    st.video(video_url, start_time=0, format="video/mp4")
-
 # Date Selection
 if not df.empty:
     unique_dates = df['Date'].dt.strftime("%d %B %Y").unique()
-    selected_date = st.selectbox("Select RomRom Trip Date", unique_dates, index=0)
+    selected_date = st.selectbox("Select a RomRom Trip Date", unique_dates, index=0)
 
-    if selected_date != "Select RomRom Trip Date":
+    if selected_date != "Select a RomRom Trip Date":
         st.markdown("---")  # Add separator line
 
         # Filter data based on selected date
@@ -71,14 +65,17 @@ if not df.empty:
         if not selected_data.empty:
             st.markdown(f'<p class="selected-date">Selected Date: {selected_date}</p>', unsafe_allow_html=True)
             for index, row in selected_data.iterrows():
-                with st.expander(
-                    f"<p class='expander-header'>{row['Date'].strftime('%d %B %Y')} - {row['destination']}</p>",
-                ):
-                    st.markdown(f"<p class='details-item'><strong>Source:</strong> {row['source']}</p>", unsafe_allow_html=True)
-                    st.markdown(f"<p class='details-item'><strong>Mode:</strong> {row['mode']} ({row['mode_value']})</p>", unsafe_allow_html=True)
-                    st.markdown(f"<p class='details-item'><strong>Where to Stay:</strong> {row['where_to_stay']}</p>", unsafe_allow_html=True)
-                    st.markdown(f"<p class='details-item'><strong>Places to Visit:</strong> {row['places_to_visit']}</p>", unsafe_allow_html=True)
-                    st.markdown(f"<p class='budget'><strong>Total Budget at Destination:</strong> {row['BUDGET']}</p>", unsafe_allow_html=True)
+                st.markdown(
+                    f"<div class='trip-details'>"
+                    f"<p class='expander-header'>{row['Date'].strftime('%d %B %Y')} - {row['destination']}</p>"
+                    f"<p class='details-item'><strong>Source:</strong> {row['source']}</p>"
+                    f"<p class='details-item'><strong>Mode:</strong> {row['mode']} ({row['mode_value']})</p>"
+                    f"<p class='details-item'><strong>Where to Stay:</strong> {row['where_to_stay']}</p>"
+                    f"<p class='details-item'><strong>Places to Visit:</strong> {row['places_to_visit']}</p>"
+                    f"<p class='budget'><strong>Total Budget at Destination:</strong> {row['BUDGET']}</p>"
+                    f"</div>",
+                    unsafe_allow_html=True,
+                )
 
         else:
             st.warning("No information available for the selected date.")
